7f1170177879f939e094dae8b20d008a35ba99bd
lol-cubes <arinmkhare@gmail.com>
2019-06-28 13:14:50 -0400

    created Competition class to hold comp info

--- comp_finder.py
+++ comp_finder.py
@@ -10,6 +10,7 @@ from selenium.webdriver import Chrome, ChromeOptions
 import os
 import time
 
+
 PAGE_URL = 'https://www.worldcubeassociation.org/competitions?utf8=%E2%9C%93&region=USA&search=&state=present&year=all+years&from_date=&to_date=&delegate=&display=list'
 cwd = os.getcwd()
 
@@ -31,5 +32,49 @@ op = ChromeOptions()
 op.add_argument('headless')
 driver = Chrome(f'{cwd}/chromedriver', options=op)
 
+
+class Competition:
+    """Object to hold all information about a WCA competition"""
+    def __init__(self, date, location, url, venue):
+        # all attributes defined here can be found in the WCA competitions page
+        # the @property methods find information that must be found on the competition's individual page.
+        self.date = date
+        self.location = location
+        self.url = url
+        self.venue = venue
+
+
+    @property
+    def reached_competitor_limit(self):
+        """tells if the competitor limit for the competition has been reached
+        
+        Returns:
+            bool -- True if it has been reached, False if not
+        """
+        pass
+
+
+    @property
+    def driving_distance(self, current_location):
+        """Finds the driving distance from your location to the competition
+        
+        Args:
+            current_location -- address of where you want to drive to the competition from
+
+        Returns:
+            str -- hours h minutes min
+        """
+        pass
+
+
+    @property
+    def text(self):
+        """Creates message containing complete information
+        
+        Message to be written to text file with other competitions
+        """
+        pass
+
 driver.get(PAGE_URL)
 
+competitions_elements = driver.find_elements_by_xpath('//li[@class="list-group-item not-past"]')
