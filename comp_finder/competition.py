4716598471603d7725ae73012346d5c38cc37fac
lol-cubes <arinmkhare@gmail.com>
2019-06-28 16:29:09 -0400

    added competitor limit function

--- comp_finder/competition.py
+++ comp_finder/competition.py
@@ -1,6 +1,12 @@
+# various XPaths for competition info elements
+COMPETITOR_LIMIT = '//*[@id="registration_requirements_text"]/text()[3]'
+COMPETITORS_LIST = '//*[@id="competition-nav"]/div/a[3]'
+COMPETITOR_TABLE_FOOTER = '//*[@id="competition-data"]/div/div[1]/div[2]/div[2]/table/tfoot/tr/td[1]'
+
+
 class Competition:
     """Object to hold all information about a WCA competition"""
-    def __init__(self, name, date, location, url, venue, driver):
+    def __init__(self, name, date, location, url, venue, d):
         # all attributes defined here can be found in the WCA competitions page
         # the @property methods find information that must be found on the competition's individual page.
         self.name = name
@@ -10,7 +16,7 @@ class Competition:
         self.venue = venue
         
         # necessary for finding other info about competition
-        self.driver = driver
+        self.driver = d
 
 
     @property
@@ -20,7 +26,26 @@ class Competition:
         Returns:
             bool -- True if it has been reached, False if not
         """
-        return ''
+        with self.driver as driver:
+            driver.get(self.url)
+            
+            # find the line where the competitor limit is stated
+            competitor_limit_line = driver.find_element_by_xpath(COMPETITOR_LIMIT)
+            competitor_limit = int(competitor_limit_line.split(' ')[-2])
+            # find the link to the competitor list
+            competitor_list_url = driver.find_element_by_xpath(COMPETITORS_LIST).get_attribute('href')
+
+            driver.get(competitor_list_url)
+
+            # find bottom text of competitor table
+            table_footer = driver.find_element_by_xpath(COMPETITOR_TABLE_FOOTER).text.strip()
+
+            people = int(table_footer.split(' ')[-2])
+
+            if people == competitor_limit:
+                return True
+            else:
+                return False
 
 
     @property
--- comp_finder/find_comps.py
+++ comp_finder/find_comps.py
@@ -1,74 +1,73 @@
-#!/Library/Frameworks/Python.framework/Versions/3.7/bin/python3
-
-"""Checks WCA competitions (in the USA) and records information about ones close to you.
-
-Usage:
-python comp_finder.py current_location state1 [state2 state3 ...]
-"""
-
-from selenium.webdriver import Chrome, ChromeOptions
-from comp_finder.competition import Competition
-import os
-import time
+def find_comps():
+    from selenium.webdriver import Chrome, ChromeOptions
+    from comp_finder.competition import Competition
+    import os
+    import time
 
 
-PAGE_URL = 'https://www.worldcubeassociation.org/competitions?utf8=%E2%9C%93&region=USA&search=&state=present&year=all+years&from_date=&to_date=&delegate=&display=list'
-cwd = os.getcwd()
+    PAGE_URL = 'https://www.worldcubeassociation.org/competitions?utf8=%E2%9C%93&region=USA&search=&state=present&year=all+years&from_date=&to_date=&delegate=&display=list'
+    cwd = os.getcwd()
 
-# finds nearby states
-with open(f'{cwd}/states.txt', 'r') as f:
-    states = f.read().split('\n')
+    # finds nearby states
+    with open(f'{cwd}/states.txt', 'r') as f:
+        states = f.read().split('\n')
 
-if states == []:
-    import sys
-
-    states = sys.argv[2:]
-
-    # if states not specified when script was run
     if states == []:
-        raise Exception('No states inputted')
+        import sys
 
-    with open(f'{cwd}/states.txt', 'w+') as f:
-        f.write('\n'.join(states))
+        states = sys.argv[2:]
+
+        # if states not specified when script was run
+        if states == []:
+            raise Exception('No states inputted')
+
+        with open(f'{cwd}/states.txt', 'w+') as f:
+            f.write('\n'.join(states))
 
 
-# create webdriver without physical window
-op = ChromeOptions()
-op.add_argument('headless')
-driver = Chrome(f'{cwd}/chromedriver', options=op)
+    # create webdriver without physical window
+    op = ChromeOptions()
+    op.add_argument('headless')
+    driver = Chrome(f'{cwd}/chromedriver', options=op)
 
 
-# Various XPaths for competition info elements
-COMPETITION = '//li[@class="list-group-item not-past"]'
-LOCATION = './/div[@class="location"]'
-LINK = './/div[@class="competition-link"]/a'
-DATE = './/span[@class="date"]'
-VENUE = './/div[@class="venue-link"]/p'
-
-driver.get(PAGE_URL)
-
-# finds all list elements for competitions on competitions page
-competitions_elements = driver.find_elements_by_xpath(COMPETITION)
-
-# list to be filled with Competition objects
-competitions = []
-
-for elem in competitions_elements:
-    comp_location = elem.find_element_by_xpath(LOCATION).text
+    # Various XPaths for competition info elements
+    COMPETITION = '//li[@class="list-group-item not-past"]'
+    LOCATION = './/div[@class="location"]'
+    LINK = './/div[@class="competition-link"]/a'
+    DATE = './/span[@class="date"]'
+    VENUE = './/div[@class="venue-link"]/p'
     
-    # checks if competition is in any of the local states
-    if comp_location.split(', ')[-1] in states:
-        # find various info about competition
+    # update output.txt every 30 minutes
+    while True:
 
-        comp_name = elem.find_element_by_xpath(LINK).text
-        comp_date = elem.find_element_by_xpath(DATE).text
-        comp_venue = elem.find_element_by_xpath(VENUE).text
-        comp_link = elem.find_element_by_xpath(LINK).get_attribute('href')
+        driver.get(PAGE_URL)
 
-        competitions.append(
-            Competition(comp_name, comp_date, comp_location, 
-                              comp_link, comp_venue, driver)
-            )
+        # finds all list elements for competitions on competitions page
+        competitions_elements = driver.find_elements_by_xpath(COMPETITION)
 
-    with open(f'{cwd}/output.txt', 'w+') as f:
-        f.write('\n\n'.join([str(i) for i in competitions]))
+        # list to be filled with Competition objects
+        competitions = []
+
+        for elem in competitions_elements:
+            comp_location = elem.find_element_by_xpath(LOCATION).text
+            
+            # checks if competition is in any of the local states
+            if comp_location.split(', ')[-1] in states:
+                # find various info about competition
+
+                comp_name = elem.find_element_by_xpath(LINK).text
+                comp_date = elem.find_element_by_xpath(DATE).text
+                comp_venue = elem.find_element_by_xpath(VENUE).text
+                comp_link = 'https://www.worldcubeassociation.org' + \
+                    elem.find_element_by_xpath(LINK).get_attribute('href')
+
+                competitions.append(
+                    Competition(comp_name, comp_date, comp_location, 
+                                    comp_link, comp_venue, driver)
+                    )
+            
+            time.sleep(900)
+
+        with open(f'{cwd}/output.txt', 'w+') as f:
+            f.write('\n\n'.join([str(i) for i in competitions]))
