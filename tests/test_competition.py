49b23b4f3ee0887b927ad495856c81e9a4cef460
lol-cubes <arinmkhare@gmail.com>
2019-06-28 16:41:09 -0400

    added unit test modules

---
+++ tests/test_competition.py
@@ -0,0 +1,21 @@
+from comp_finder import competition
+import unittest
+
+
+class TestCompetition(unittest.TestCase):
+
+    def test_reached_competitor_limit(self):
+        pass
+
+    def test_venue_address(self):
+        pass
+
+    def test_driving_distance(self):
+        pass
+
+    def test_message(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ tests/test_find_comps.py
@@ -0,0 +1,12 @@
+from comp_finder import find_comps
+import unittest
+
+
+class TestCompFinder(unittest.TestCase):
+
+    def test_comp_finder(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
